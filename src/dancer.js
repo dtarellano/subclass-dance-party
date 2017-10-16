0cf29e947ea8cb5fc705f2e0354b33cbb5714a40
dtarellano <dtarellano1@gmail.com>
2017-10-16 15:37:18 -0700

    Implemented makeDancer and makeBlinkyDancer instantiation. Failing the dance test.

--- src/dancer.js
+++ src/dancer.js
@@ -1,17 +1,16 @@
 // Creates and returns a new dancer object that can step
 var makeDancer = function(top, left, timeBetweenSteps) {
-
+  debugger;
   this.top = top;
   this.left = left;
   this.timeBetweenSteps = timeBetweenSteps;
   // use jQuery to create an HTML <span> tag
   this.$node = $('<span class="dancer"></span>');
-
   this.step();
 
   // now that we have defined the dancer object, we can start setting up important parts of it by calling the methods we wrote
   // this one sets the position to some random default point within the body
-  this.setPosition(this.top, this.left);
+  this.setPosition(this.$node, this.top, this.left);
 };
 
 makeDancer.prototype.step = function() {
