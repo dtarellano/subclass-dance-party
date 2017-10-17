609b66e9dac6509c699bd513c0dbdf69f589b971
dtarellano <dtarellano1@gmail.com>
2017-10-16 17:15:23 -0700

    Fixed shakyDancer's left position during animation.

--- src/shakyDancer.js
+++ src/shakyDancer.js
@@ -8,7 +8,13 @@ makeShakyDancer.prototype.constructor = makeShakyDancer;
 makeShakyDancer.prototype.step = function() {
   makeDancer.prototype.step.call(this);
 
-  this.$node.animate({'left': '+10%'}, 'slow', 'swing', function() {
-    $(this).animate({'left': '-5%'}, 'slow');
+  var position1 = this.left + 10;
+  position1 = position1.toString() + 'px';
+
+  var position2 = this.left - 5;
+  position2 = position2.toString() + 'px';
+
+  this.$node.animate({'left': position1}, 'fast', 'swing', function() {
+    $(this).animate({'left': position2}, 'fast');
   });
 };
